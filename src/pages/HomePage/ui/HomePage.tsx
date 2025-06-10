ed8a2a5967c412f15c830781f54a69d88ee7fe30
Gosudarev Danila <danidagosudarev@gmail.com>
2025-06-11 01:11:22 +0300

    add home info about project

--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -1,9 +1,7 @@
 import type { FC } from "react";
 
+import { HomeInfo } from "@/widgets/HomeInfo";
+
 export const HomePage: FC = () => {
-  return (
-    <div>
-      <div>HomePage</div>
-    </div>
-  );
+  return <HomeInfo />;
 };
