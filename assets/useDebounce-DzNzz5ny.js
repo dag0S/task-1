4d81abc59e80b49f4c104f2356a6cee3c3923a4f
Gosudarev Danila <danidagosudarev@gmail.com>
2025-06-15 22:47:14 +0300

    Updates

--- assets/useDebounce-DzNzz5ny.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-EVK1cD6X.js";const n=(e,t)=>{const[r,s]=o.useState(e);return o.useEffect(()=>{const u=setTimeout(()=>{s(e)},t);return()=>{clearTimeout(u)}},[e,t]),r};export{n as u};
